c01012d00d518ace1cb09e5aa5a85d92f3f32499
mkomlaetou <m.komlaetou@gmail.com>
2024-05-07 02:32:39 +0000

    migrate from managed_policy_arn to aws_iam_role_policy_attachment

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ resource "aws_iam_role" "role" {
   name               = var.role_details.role_name
   assume_role_policy = local.crossing_account_ids != [] ? data.aws_iam_policy_document.cross_account_policy[0].json : local.principals != [] ? data.aws_iam_policy_document.service_account_policy[0].json : file("${path.cwd}${var.role_details.custom_sts_policy_file_path}")
 
-  managed_policy_arns = var.role_details.managed_policies != [] ? local.policies : null
+  #managed_policy_arns = var.role_details.managed_policies != [] ? local.policies : null
   dynamic "inline_policy" {
     for_each = var.role_details.permission_policy_file_path
     content {
@@ -92,3 +92,10 @@ resource "aws_iam_instance_profile" "ec2" {
   role  = aws_iam_role.role.name
 }
 
+
+resource "aws_iam_role_policy_attachment" "managed" {
+  for_each = toset(local.policies)
+  role = aws_iam_role.role.id
+  policy_arn = each.value
+
+}
